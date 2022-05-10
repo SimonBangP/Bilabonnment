356b59a31b40f87216f50c6962f5d26305947630
simonbang <simo12k1@stud.kea.dk>
2022-05-10 11:44:23 +0200

    Initial commit

---
+++ src/test/java/com/example/bilabonnmenteksamensprojekt/BilabonnmentEksamensprojektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bilabonnmenteksamensprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilabonnmentEksamensprojektApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
