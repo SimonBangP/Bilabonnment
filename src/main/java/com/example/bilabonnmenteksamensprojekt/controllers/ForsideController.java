8bc673d99416b07739cd0f3e6a78df16c0b75163
simonbang <simo12k1@stud.kea.dk>
2022-05-25 17:42:42 +0200

    Oprettet Tickets.

--- src/main/java/com/example/bilabonnmenteksamensprojekt/controllers/ForsideController.java
+++ src/main/java/com/example/bilabonnmenteksamensprojekt/controllers/ForsideController.java
@@ -1,10 +1,14 @@
 package com.example.bilabonnmenteksamensprojekt.controllers;
 
+import com.example.bilabonnmenteksamensprojekt.models.tickets.Ticket;
+import com.example.bilabonnmenteksamensprojekt.services.TicketService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 public class ForsideController {
@@ -20,4 +24,5 @@ public class ForsideController {
             return "redirect:/?location=forside";
         }
     }
+
 }
